42f884d2cf5ba186b5170b496575228b77ef9380
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-04-12 18:14:16 -0400

    Added in eslint with typescript and React rulesets.

--- src/Login/login.tsx
+++ src/Login/login.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React from 'react';
+import { Form } from 'react-bootstrap';
 
 interface loginProps {
 
 }
 
-export const login: React.FC<loginProps> = ({}) => {
-        return (
-            <Form>
-                <Form.Group controlId="institutionalEmail">
-                    <Form.Label>Institutional Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="cxavier@xavierinstitute.edu"></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="******"></Form.Control>
-                </Form.Group>
-            </Form>
-        );
-}
+export const login: React.FC<loginProps> = () => (
+    <Form>
+        <Form.Group controlId="institutionalEmail">
+            <Form.Label>Institutional Email Address</Form.Label>
+            <Form.Control type="email" placeholder="cxavier@xavierinstitute.edu"></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="******"></Form.Control>
+        </Form.Group>
+    </Form>
+);
 
 export default login;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
-import Login from "./Login/login"
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Login/login';
 import HomePage from './Home/HomePage';
 
 interface RouterProps {
 
 }
 
-export const Router: React.FC<RouterProps> = ({}) => {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <HomePage/>
-                    </Route>
-                    <Route path="/login">
-                        <Login/>
-                    </Route>
-                    <Route path="/user">
-                        <User/>
-                    </Route>
-                    <Route path="/">
-                        <NoPage/>
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        );
-}
+export const Router: React.FC<RouterProps> = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage/>
+                </Route>
+                <Route path="/login">
+                    <Login/>
+                </Route>
+                <Route path="/user">
+                    <User/>
+                </Route>
+                <Route path="/">
+                    <NoPage/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 function User() {
     return <h2>User</h2>;
