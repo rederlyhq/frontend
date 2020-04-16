9080e58957c7afe5376ad0dd6d8debea9abf8082
Panopticon Rising <panopticonrising@protonmail.com>
2020-04-15 22:07:12 -0400

    Updated functionality to put a Login Modal on the Home Page, rather than as a separate page.
    Chose to use react-bootstrap rather than react-hook-forms because the styling came with it.
    Fixed the proxying CORS problem in the package.json (Tom++)
    Added in Axios and set up a basic login call against the server endpoint.

--- src/Login/loginForm.tsx
+++ src/Login/loginForm.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import AxiosRequest from '../Hooks/AxiosRequest';
 
 interface LoginFormProps {
 
@@ -11,29 +11,73 @@ type LoginFormData = {
     password: string;
 }
 
+/**
+ * This component renders the Login form, and should redirect the user if login succeeds.
+ * 
+ * Suggestion: Could useEffect to prevalidate institutional emails?
+ */
 export const LoginForm: React.FC<LoginFormProps> = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
+    const [validated, setValidated] = useState(false);
+    const [loginError, setLoginError] = useState('');
 
-    const onSubmit = (data: any) => (console.log(data));
+    const handleLogin = async () => {
+        try {
+            const resp = await AxiosRequest.post('/users/login', {});
+            console.log(resp.data);
 
-    console.log(watch('email'));
+            setLoginError(resp.data.msg);
+            // TODO: Redirect to Course List page.
+        } catch (err) {
+            setLoginError('A network error occurred. Please try again later.');
+        }
+    };
+
+    const handleSubmit = (event: any) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            handleLogin();
+        }
+  
+        setValidated(true);
+    };
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit} action='#'>
             <Form.Group controlId="institutionalEmail">
+                {(loginError !== '') && <Alert variant="danger">{loginError}</Alert>}
                 <Form.Label>Institutional Email Address</Form.Label>
-                <Form.Control name="email" autoComplete="username" type="email" placeholder="cxavier@xavierinstitute.edu" ref={register} />
+                <Form.Control
+                    required
+                    defaultValue='' 
+                    name="email" 
+                    autoComplete="username" 
+                    type="email" 
+                    placeholder="cxavier@xavierinstitute.edu"
+                    isValid={false}
+                />
+                <Form.Control.Feedback type="invalid">{<span>An Institutional is required.</span>}</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control name="password" autoComplete="current-password" type="password" placeholder="******" ref={register} />
+                <Form.Control
+                    required
+                    defaultValue='' 
+                    name="password" 
+                    autoComplete="current-password" 
+                    type="password" 
+                    placeholder="******" 
+                />
+                <Form.Control.Feedback type="invalid">{<span>A password is required.</span>}</Form.Control.Feedback>
             </Form.Group>
-            {errors.password && <span>A password is required.</span>}
             <Form.Group>
                 <Button type="submit">Submit</Button>
             </Form.Group>
         </Form>
     );
-}
+};
 
 export default LoginForm;
