415bdba6a175fb5d76a670d1c700bb8375cc381e
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-05-16 15:51:51 -0400

    Added an animated intro to the create course page.

--- src/Courses/CourseCreation/CourseCreationPage.tsx
+++ src/Courses/CourseCreation/CourseCreationPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ICourseTemplate } from '../CourseInterfaces';
 import CourseTemplateList from './CourseTemplateList';
 import { FormControl } from 'react-bootstrap';
+import { motion } from 'framer-motion';
 
 interface CourseCreationPageProps {
 
@@ -31,13 +32,34 @@ export const CourseCreationPage: React.FC<CourseCreationPageProps> = ({}) => {
             template.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
         )));
     };
+    
+    const transition = {
+        duration: 1,
+        ease: [0.43, 0.13, 0.23, 0.96]
+    };
+
+    const anim = {
+        enter: {
+            x: 1000,
+            opacity: 0,
+            transition
+        },
+        exit: {
+            x: -1000,
+            opacity: 0
+        },
+        center: {
+            x: 0,
+            opacity: 1
+        }
+    };
 
     return (
-        <>
+        <motion.div variants={anim} initial="enter" animate="center" exit="exit">
             <h1>Select a course template:</h1>
             <FormControl type="search" placeholder="Search by Course or Curriculum Name" onChange={filterCourseTemplates} />
             <CourseTemplateList courseTemplates={filteredCourseTemplates} />
-        </>
+        </motion.div>
     );
 };
 
