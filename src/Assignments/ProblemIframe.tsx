cdc564a8d6125b175ce13063662b13f7772c3079
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-06-06 16:48:22 -0400

    Refactored Problem Page design, improved smoothness of rendering.

--- src/Assignments/ProblemIframe.tsx
+++ src/Assignments/ProblemIframe.tsx
@@ -6,42 +6,68 @@ interface ProblemIframeProps {
     problem: ProblemObject;
 }
 
+/**
+ * The most important part- rendering the problem.
+ * We used the document.write strategy before for backwards compatibility, but modern browsers now block it.
+ * We _could_ also set the form to just render the URL directly from the server, but this provides more flexibility
+ * with further work on the JSON data.
+ * Important reference: https://medium.com/the-thinkmill/how-to-safely-inject-html-in-react-using-an-iframe-adc775d458bc
+ */
 export const ProblemIframe: React.FC<ProblemIframeProps> = ({problem}) => {
-    const iframeRef = useRef(null);
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const [renderedHTML, setRenderedHTML] = useState<string>('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
+    const [height, setHeight] = useState('100vh');
 
     useEffect(()=>{
         setLoading(true);
         (async () => {
             try {
                 const res = await AxiosRequest.get(`/courses/question/${problem.id}`);
-                console.log(res.data);
-
-                if (!iframeRef) return;
-                const current: any = iframeRef.current;
-                console.log(current);
-                if (!current) return;
-                const iframeDoc: any = current.contentDocument;
-                console.log(iframeDoc);
-                if (!iframeDoc) return;
-                iframeDoc.open();
-                iframeDoc.write(res.data.data.rendererData.renderedHTML);
-                iframeDoc.close();
+                // TODO: Error handling.
+                setRenderedHTML(res.data.data.rendererData.renderedHTML);
             } catch (e) {
                 setError(e.message);
                 console.error(e);
-            } finally {
                 setLoading(false);
             }
         })();
-    }, [iframeRef]);
+    }, [problem.id]);
+
+    const setScrollHeight = () => {
+        const iframeDoc = iframeRef.current?.contentDocument;
+
+        const body = iframeDoc?.body;
+        if (body === undefined) {
+            console.log('Couldn\'t access body of iframe');
+            return;
+        }
+
+        body.onresize = () => {
+            const scrollHeight = iframeDoc?.body.scrollHeight;
+            if (!scrollHeight) {
+                console.log('Problem iframe did not return a valid height on load.');
+                return;
+            }
+            setHeight(`${scrollHeight}px`);
+        };
+        setLoading(false);
+    };
 
     return (
         <>
-            { loading && <div>Loading...</div> }
-            { error && <div>{error}</div>}
-            <iframe ref={iframeRef} style={{width: '100%', height: '100vh', border: 'none', display: loading ? 'none' : 'block'}} />
+            { loading && <div>Loading...</div>}
+            {error ? <div>{error}</div> :
+                <iframe 
+                    title='Problem Frame'
+                    ref={iframeRef} 
+                    style={{width: '100%', height: height, border: 'none', visibility: loading ? 'hidden' : 'visible'}}
+                    sandbox='allow-same-origin allow-forms allow-scripts allow-popups'
+                    srcDoc={renderedHTML}
+                    onLoad={setScrollHeight}
+                />
+            }
         </>
     );
 };
