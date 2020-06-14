ff5a6be6f1b2999d702f73daa10f9b4b13a34c4c
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-06-13 22:46:33 -0400

    Updated dropdowns to show the selected object's name.

--- src/Components/SubObjectDropdown.tsx
+++ src/Components/SubObjectDropdown.tsx
@@ -7,17 +7,18 @@ interface SubObjectDropdownProps {
     title: string;
     subObjArray: Array<any>;
     style?: any;
+    eventKey: string;
 }
 
 /**
  * This Dropdown button renders a dynamic list of selectable subobject names.
  * Intended use case: List of 
  */
-export const SubObjectDropdown: React.FC<SubObjectDropdownProps> = ({eventKeyState, title, subObjArray, style}) => {
+export const SubObjectDropdown: React.FC<SubObjectDropdownProps> = ({eventKeyState, title, eventKey, subObjArray, style}) => {
     return (
-        <NavDropdown title={title} id={`${title}-dropdown`} active={_.startsWith(eventKeyState, title)} style={style} drop='right'>
+        <NavDropdown title={title} id={`${eventKey}-dropdown`} active={_.startsWith(eventKeyState, eventKey)} style={style} drop='right'>
             {subObjArray.map(obj => (
-                <NavDropdown.Item key={obj.id} eventKey={`${title}-${obj.id}`}>{obj.name}</NavDropdown.Item>
+                <NavDropdown.Item key={obj.id} eventKey={`${eventKey}-${obj.id}`}>{obj.name}</NavDropdown.Item>
             ))}
         </NavDropdown>
     );
