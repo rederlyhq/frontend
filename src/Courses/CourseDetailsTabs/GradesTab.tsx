ec8e6d0e8d54ba6473f4f1c9ea050ee58c411b6e
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-06-13 22:18:57 -0400

    Added generic dropdown component, nice UI flow.

--- src/Courses/CourseDetailsTabs/GradesTab.tsx
+++ src/Courses/CourseDetailsTabs/GradesTab.tsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import StudentGradesList from './StudentGradesList';
 import AxiosRequest from '../../Hooks/AxiosRequest';
+import { Nav, Table, DropdownButton, NavDropdown } from 'react-bootstrap';
+import GradeTable from './GradeTable';
+import _ from 'lodash';
+import SubObjectDropdown from '../../Components/SubObjectDropdown';
+import { UnitObject, TopicObject, ProblemObject } from '../CourseInterfaces';
 
 interface GradesTabProps {
     courseId: number
+}
+
+enum GradesView {
+    OVERVIEW = 'Overview',
+    UNITS = 'Units',
+    TOPICS = 'Topics',
+    PROBLEMS = 'Problems'
+}
+
+interface IDropdownCascade {
+    unit?: UnitObject,
+    topic?: TopicObject,
+    problem?: ProblemObject
 }
 
 /**
@@ -14,6 +32,32 @@ interface GradesTabProps {
  */
 export const GradesTab: React.FC<GradesTabProps> = ({courseId}) => {
     const [selfGrades, setSelfGrades] = useState([]);
+    const [view, setView] = useState<string>('Units');
+    const [selectedObjects, setSelectedObjects] = useState<IDropdownCascade>({});
+
+    const mockUnitsData = [
+        {name: 'Tom', average: '10%', lowest: '0%', id: 1},
+        {name: 'Tom', average: '10%', lowest: '0%', id: 2},
+        {name: 'Tom', average: '10%', lowest: '0%', id: 3},
+        {name: 'Tom', average: '10%', lowest: '0%', id: 4}
+    ];
+
+    const handleChangedView = (selectedView: string) => {
+        console.log('handling changing view', selectedView);
+        setView(selectedView);
+        if (selectedView === GradesView.OVERVIEW) {
+            setSelectedObjects({});
+        } else if (_.startsWith(selectedView, GradesView.UNITS)) {
+            let mockUnit = new UnitObject({name: 'Mock Unit'});
+            setSelectedObjects({unit: mockUnit});
+        } else if (_.startsWith(selectedView, GradesView.TOPICS)) {
+            let mockTopic = new TopicObject({name: 'Mock Topic'});
+            setSelectedObjects({unit: selectedObjects.unit, topic: mockTopic});
+        } else if (_.startsWith(selectedView, GradesView.PROBLEMS)) {
+            let mockProblem = new ProblemObject({problemNumber: 1});
+            setSelectedObjects({...selectedObjects, problem: mockProblem});
+        }
+    };
 
     useEffect(()=>{
         (async () => {
@@ -28,11 +72,22 @@ export const GradesTab: React.FC<GradesTabProps> = ({courseId}) => {
         })();
     }, []);
 
+    console.log(selectedObjects);
+
     return (
-        <div>
-            {/* TODO: List Course Summary Grades */}
-            <StudentGradesList grades={selfGrades}/>
-        </div>
+        <>
+            <Nav fill variant='pills' activeKey={view} onSelect={(selectedKey: string) => handleChangedView(selectedKey)}>
+                <Nav.Item>
+                    <Nav.Link eventKey={GradesView.OVERVIEW}>
+                        Overview
+                    </Nav.Link>
+                </Nav.Item>
+                <SubObjectDropdown title={GradesView.UNITS} eventKeyState={view} subObjArray={[{name: 'Unit 1', id: 1}]}/>
+                <SubObjectDropdown title={GradesView.TOPICS} eventKeyState={view} subObjArray={[{name: 'Topic 1', id: 1}]} style={{visibility: selectedObjects.unit ? 'visible' : 'hidden'}}/>
+                <SubObjectDropdown title={GradesView.PROBLEMS} eventKeyState={view} subObjArray={[{name: 'Problem 1', id: 1}]} style={{visibility: selectedObjects.topic ? 'visible' : 'hidden'}}/>
+            </Nav>
+            <GradeTable grades={mockUnitsData}/>
+        </>
     );
 };
 
