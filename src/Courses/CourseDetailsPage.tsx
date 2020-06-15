c76e10e5531fdb612d57e5a93b3735e6873ede69
tommylettieri <tommylettieri@gmail.com>
2020-06-14 21:18:15 -0400

    Move enrollment link to enrollments tab

--- src/Courses/CourseDetailsPage.tsx
+++ src/Courses/CourseDetailsPage.tsx
@@ -27,8 +27,6 @@ export const CourseDetailsPage: React.FC<CourseDetailsPageProps> = () => {
     const { courseId } = useParams();
     const [course, setCourse] = useState<any>({});
     const [activeTab, setActiveTab] = useState<CourseDetailsTabs>(CourseDetailsTabs.DETAILS);
-    const textAreaRef = useRef<FormControl<'input'> & HTMLInputElement>(null);
-    const enrollUrl: string = `${window.location.host}/common/courses/enroll/${course?.code}`;
 
     useEffect(() => {
         (async () => {
@@ -41,25 +39,6 @@ export const CourseDetailsPage: React.FC<CourseDetailsPageProps> = () => {
     }, [courseId]);
 
     if (!courseId) return <div>Please return to login.</div>;
-
-    const copyToClipboard = (e: any) => {
-        if (textAreaRef?.current === null) {
-            console.error('enrollLinkRef not logged properly.');
-            return;
-        }
-        console.log(textAreaRef);
-        textAreaRef?.current.select();
-
-        try {
-            const res = document.execCommand('copy');
-            console.log(`Copy operation ${res ? 'was successful' : 'failed'}`);
-        } catch (err) {
-            console.error(err);
-        } finally {
-            e.target.focus();
-        }
-
-    };
 
     return (
         <Container>
@@ -97,20 +76,6 @@ export const CourseDetailsPage: React.FC<CourseDetailsPageProps> = () => {
                                     }]
                                 }
                             }} />
-
-                            <FormLabel>Enrollment Link:</FormLabel>
-                            <InputGroup className="mb-3">
-                                <FormControl
-                                    readOnly
-                                    aria-label="Enrollment link"
-                                    aria-describedby="basic-addon2"
-                                    ref={textAreaRef}
-                                    value={`http://${enrollUrl}`}
-                                />
-                                <InputGroup.Append>
-                                    <Button variant="outline-secondary" onClick={copyToClipboard}>Copy to Clipboard</Button>
-                                </InputGroup.Append>
-                            </InputGroup>
                         </>
                     )
                     }
@@ -119,7 +84,7 @@ export const CourseDetailsPage: React.FC<CourseDetailsPageProps> = () => {
                     <TopicsTab course={course} />
                 </Tab>
                 <Tab eventKey={CourseDetailsTabs.ENROLLMENTS} title="Enrollments">
-                    <EnrollmentsTab courseId={parseInt(courseId, 10)} />
+                    <EnrollmentsTab courseId={parseInt(courseId, 10)} courseCode={course.code} />
                 </Tab>
                 <Tab eventKey={CourseDetailsTabs.GRADES} title="Grades">
                     <GradesTab course={course} />
--- src/Courses/CourseDetailsTabs/EmailComponentWrapper.tsx
+++ src/Courses/CourseDetailsTabs/EmailComponentWrapper.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import CourseUsersList from './CourseUsersList';
 import { UserObject } from '../CourseInterfaces';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, FormLabel, InputGroup, FormControl } from 'react-bootstrap';
 import { userContext } from '../../NavWrapper/NavWrapper';
 import EmailModal from './EmailModal';
 import { UserRole, getUserRole } from '../../Enums/UserRole';
@@ -15,22 +15,15 @@ interface EmailComponentWrapperProps {
 /**
  * This component manages the state for the Email Students functionality.
  */
-export const EmailComponentWrapper: React.FC<EmailComponentWrapperProps> = ({users}) => {
+export const EmailComponentWrapper: React.FC<EmailComponentWrapperProps> = ({ users }) => {
     const [selectedStudents, setSelectedStudents] = useState<Array<Set<number>>>([new Set()]);
     const [showModal, setShowModal] = useState(false);
     const userType: UserRole = getUserRole(Cookies.get(CookieEnum.USERTYPE));
 
     return (
         <>
-            <Row>
-                <Col md={10}>
-                    <h2>Current Enrollments</h2>
-                </Col>
-                <Col md={2}>
-                    {userType === UserRole.PROFESSOR && <Button className="email float-right" onClick={() => setShowModal(true)}>Email Students</Button>}
-                    <EmailModal show={showModal} setClose={() => setShowModal(false)} users={selectedStudents[0]} />
-                </Col>
-            </Row>
+            {userType === UserRole.PROFESSOR && <Button className="email float-right" onClick={() => setShowModal(true)}>Email Students</Button>}
+            <EmailModal show={showModal} setClose={() => setShowModal(false)} users={selectedStudents[0]} />
             <CourseUsersList users={users} setActive={setSelectedStudents} activeUsers={selectedStudents} />
         </>
     );
