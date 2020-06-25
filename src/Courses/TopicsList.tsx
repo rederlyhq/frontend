2c3ab659d4fb771d6d2d3c4fc4830cf2b2b02b11
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-06-24 22:09:23 -0400

    Added in handling for professor/student, added in call to handle date change feedback.

--- src/Courses/TopicsList.tsx
+++ src/Courses/TopicsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ListGroup, ListGroupItem, Row, Col, Button, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
 import { TopicObject, NewCourseTopicObj } from './CourseInterfaces';
 import { BsPencilSquare } from 'react-icons/bs';
@@ -6,6 +6,10 @@ import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import AxiosRequest from '../Hooks/AxiosRequest';
+import Feedback from 'react-bootstrap/Feedback';
+import { UserRole, getUserRole } from '../Enums/UserRole';
+import Cookies from 'js-cookie';
+import { CookieEnum } from '../Enums/CookieEnum';
 
 interface TopicsListProps {
     listOfTopics: Array<NewCourseTopicObj>;
@@ -14,20 +18,25 @@ interface TopicsListProps {
     removeTopic?: _.CurriedFunction2<any, number, void>;
 }
 
-const updateTopicField = async (topicId: number, field: keyof NewCourseTopicObj, newData: Date) => {
-    console.log(`Updating Topic ${topicId} to ${field} = ${newData}`);
-    try {
-        const res = await AxiosRequest.put(`/courses/topic/${topicId}`);
-        console.log(res);
-    } catch (e) {
-        console.error(e);
-    }
-};
-
 /**
  * Lists topics. Clicking into one will go to the problem sets.
  */
 export const TopicsList: React.FC<TopicsListProps> = ({listOfTopics, flush, showEditTopic, removeTopic}) => {
+    const [topicFeedback, setTopicFeedback] = useState({topicId: -1, feedback: '', variant: 'danger'});
+    const userType: UserRole = getUserRole(Cookies.get(CookieEnum.USERTYPE));
+    
+    const updateTopicField = async (topicId: number, field: keyof NewCourseTopicObj, newData: Date) => {
+        console.log(`Updating Topic ${topicId} to ${field} = ${newData}`);
+        try {
+            const res = await AxiosRequest.put(`/courses/topic/${topicId}`);
+            console.log(res);
+            setTopicFeedback({topicId: topicId, feedback: res.data.message, variant: 'success'});
+        } catch (e) {
+            console.error(e);
+            setTopicFeedback({topicId: topicId, feedback: e.message, variant: 'danger'});
+        }
+    };
+    
     return (
         <ListGroup variant={flush ? 'flush' : undefined}>
             {listOfTopics.map(topic => (
@@ -65,6 +74,7 @@ export const TopicsList: React.FC<TopicsListProps> = ({listOfTopics, flush, show
                                             type='date' 
                                             onChange={(e: any) => updateTopicField(topic.id, 'startDate', e.target.value)}
                                             defaultValue={moment(topic.startDate).format('YYYY-MM-DD')}
+                                            readOnly={userType !== UserRole.PROFESSOR}
                                         />
                                     </FormGroup>
                                 </Col>
@@ -78,11 +88,16 @@ export const TopicsList: React.FC<TopicsListProps> = ({listOfTopics, flush, show
                                             type='date' 
                                             onChange={(e: any) => updateTopicField(topic.id, 'endDate', e.target.value)}
                                             defaultValue={moment(topic.endDate).format('YYYY-MM-DD')}
+                                            readOnly={userType !== UserRole.PROFESSOR}
                                         />
+                                        {/* TODO: Add in feedback on API call.
+                                        <Feedback type='invalid'>
+                                            {topicFeedback.feedback}
+                                        </Feedback> */}
                                     </FormGroup>
                                 </Col>
                             </>
-                            )}
+                        )}
                     </Row>
                 </ListGroupItem>
             ))}
