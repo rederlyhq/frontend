bd0ccec7b595b9724afe5c10f751a161b1d1d3e8
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-06-24 20:47:37 -0400

    Added in edit topic functionality to change start and end dates.

--- src/Courses/TopicsList.tsx
+++ src/Courses/TopicsList.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, Row, Col, Button } from 'react-bootstrap';
-import { TopicObject } from './CourseInterfaces';
+import { ListGroup, ListGroupItem, Row, Col, Button, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
+import { TopicObject, NewCourseTopicObj } from './CourseInterfaces';
 import { BsPencilSquare } from 'react-icons/bs';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import AxiosRequest from '../Hooks/AxiosRequest';
 
 interface TopicsListProps {
-    listOfTopics: Array<TopicObject>;
+    listOfTopics: Array<NewCourseTopicObj>;
     flush?: boolean;
     showEditTopic?: _.CurriedFunction2<any, number, void>;
     removeTopic?: _.CurriedFunction2<any, number, void>;
 }
+
+const updateTopicField = async (topicId: number, field: keyof NewCourseTopicObj, newData: Date) => {
+    console.log(`Updating Topic ${topicId} to ${field} = ${newData}`);
+    try {
+        const res = await AxiosRequest.put(`/courses/topic/${topicId}`);
+        console.log(res);
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 /**
  * Lists topics. Clicking into one will go to the problem sets.
@@ -37,10 +49,40 @@ export const TopicsList: React.FC<TopicsListProps> = ({listOfTopics, flush, show
                                 </Col>
                             </>
                         ) : (
-                            <Link to={loc =>({pathname: `${loc.pathname}/${topic.id}`, state: {problems: topic.questions}})}>
-                                <Col>{topic.name}</Col>
-                            </Link>
-                        )}
+                            <>
+                                <Col md={6}>
+                                    <Link to={loc =>({pathname: `${loc.pathname}/${topic.id}`, state: {problems: topic.questions}})}>
+                                        <h5>{topic.name}</h5>
+                                    </Link>
+                                </Col>
+                                <Col md={3}>
+                                    <FormGroup as={Col} controlId='start'>
+                                        <FormLabel>
+                                            Start Date:
+                                        </FormLabel>
+                                        <FormControl 
+                                            required
+                                            type='date' 
+                                            onChange={(e: any) => updateTopicField(topic.id, 'startDate', e.target.value)}
+                                            defaultValue={moment(topic.startDate).format('YYYY-MM-DD')}
+                                        />
+                                    </FormGroup>
+                                </Col>
+                                <Col md={3}>
+                                    <FormGroup as={Col} controlId='end'>
+                                        <FormLabel>
+                                            End Date:
+                                        </FormLabel>
+                                        <FormControl 
+                                            required
+                                            type='date' 
+                                            onChange={(e: any) => updateTopicField(topic.id, 'endDate', e.target.value)}
+                                            defaultValue={moment(topic.endDate).format('YYYY-MM-DD')}
+                                        />
+                                    </FormGroup>
+                                </Col>
+                            </>
+                            )}
                     </Row>
                 </ListGroupItem>
             ))}
