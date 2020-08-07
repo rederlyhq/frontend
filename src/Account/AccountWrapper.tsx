473a926c0fbeab2af0d76da9ced5ad3f866915e4
PanopticonRising <gibryon@protonmail.com>
2020-08-07 13:46:50 -0400

    Pulled out details page into a separate file. Added some minor spacing styling.

--- src/Account/AccountWrapper.tsx
+++ src/Account/AccountWrapper.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Avatar, Container, Tabs, Tab, Grid } from '@material-ui/core';
-import { CookieEnum } from '../Enums/CookieEnum';
-import Cookies from 'js-cookie';
-import _ from 'lodash';
+import { Container, Tabs, Tab, Grid } from '@material-ui/core';
+import AccountDetailsPage from './AccountDetailsPage';
 
 interface AccountWrapperProps {
 
@@ -10,30 +8,37 @@ interface AccountWrapperProps {
 
 export const AccountWrapper: React.FC<AccountWrapperProps> = () => {
     const [tabKey, setTabKey] = React.useState<number>(0);
-    const userName = Cookies.get(CookieEnum.USERNAME);
-    console.log(userName);
+    
+    const renderTab = () => {
+        switch(tabKey) {
+        case 0:
+        default:
+            return <AccountDetailsPage />;
+        case 1:
+        case 2:
+            return <Grid container item spacing={3} xs={6} justify='center'><h3>Sorry! This content isn't ready yet.</h3></Grid>;
+        }
+    };
+
     return (
-        <Grid container spacing={3}>
-            <Grid xs={3}>
-                <Tabs
-                    orientation='vertical'
-                    variant='scrollable'
-                    value={tabKey}
-                    onChange={(event: React.ChangeEvent<{}>, newValue: number) => {setTabKey(newValue);}}
-                    aria-label='Account Page Navigation'
-                >
-                    <Tab label={'Details'} />
-                    <Tab label={'Settings'} />
-                    <Tab label={'Grades'} />
-                </Tabs>
+        <Container style={{marginTop: '1rem'}}>
+            <Grid container spacing={3}>
+                <Grid container item xs={3}>
+                    <Tabs
+                        orientation='vertical'
+                        variant='scrollable'
+                        value={tabKey}
+                        onChange={(event: React.ChangeEvent<{}>, newValue: number) => {setTabKey(newValue);}}
+                        aria-label='Account Page Navigation'
+                    >
+                        <Tab label={'Details'} />
+                        <Tab label={'Settings'} />
+                        <Tab label={'Grades'} />
+                    </Tabs>
+                </Grid>
+                {renderTab()}
             </Grid>
-            <Grid xs={6}>
-                <Avatar alt={`${userName}'s Avatar`} style={{height: '150px', width: '150px'}}>
-                    {/* Renders Avatars with a user's initials */}
-                    {_.chain(userName).words().map(w => w[0]).join('').value()}
-                </Avatar>
-            </Grid>
-        </Grid>
+        </Container>
     );
 };
 
