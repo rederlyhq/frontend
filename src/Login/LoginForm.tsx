bf38e4d0e38428e88f9e715b9fd9775d02ceaa4b
red-tt-bear <red.tt.bear@gmail.com>
2020-08-27 20:38:19 -0400

    RED-52 Added forgot password button

--- src/Login/LoginForm.tsx
+++ src/Login/LoginForm.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Row, Col } from 'react-bootstrap';
 import AxiosRequest from '../Hooks/AxiosRequest';
 import useAlertState from '../Hooks/useAlertState';
 import { useHistory } from 'react-router-dom';
 import Cookie from 'js-cookie';
 import { getUserRoleFromServer } from '../Enums/UserRole';
 import { CookieEnum } from '../Enums/CookieEnum';
+import ButtonAndModal from '../Components/ButtonAndModal';
 
 interface LoginFormProps {
 
@@ -23,27 +24,27 @@ type LoginFormData = {
  */
 export const LoginForm: React.FC<LoginFormProps> = () => {
     const [validated, setValidated] = useState(false);
-    const [{message: loginAlertMsg, variant: registrationAlertType}, setLoginAlertMsg] = useAlertState();
-    const [formState, setFormState] = useState<LoginFormData>({email: '', password: ''});
+    const [{ message: loginAlertMsg, variant: registrationAlertType }, setLoginAlertMsg] = useAlertState();
+    const [formState, setFormState] = useState<LoginFormData>({ email: '', password: '' });
     const history = useHistory();
 
     const handleNamedChange = (name: keyof LoginFormData) => {
         return (event: any) => {
-            if (name !== event.target.name) { 
+            if (name !== event.target.name) {
                 console.error(`Mismatched event, ${name} is on ${event.target.name}`);
             }
             const val = event.target.value;
-            setFormState({...formState, [name]: val});
+            setFormState({ ...formState, [name]: val });
         };
     };
 
     const handleLogin = async () => {
         try {
-            const resp = await AxiosRequest.post('/users/login', {email: formState.email, password: formState.password});
+            const resp = await AxiosRequest.post('/users/login', { email: formState.email, password: formState.password });
             console.log(resp.data);
 
             if (resp.status === 200) {
-                setLoginAlertMsg({message: resp.data?.msg || 'Logged in!', variant: 'success'});
+                setLoginAlertMsg({ message: resp.data?.msg || 'Logged in!', variant: 'success' });
                 // TODO: Create a User class to massage and error-handle these fields.
                 Cookie.set(CookieEnum.USERTYPE, getUserRoleFromServer(resp.data.data.roleId));
                 Cookie.set(CookieEnum.USERID, resp.data.data.userId);
@@ -53,9 +54,9 @@ export const LoginForm: React.FC<LoginFormProps> = () => {
             }
         } catch (err) {
             if (err.response?.status === 401) {
-                setLoginAlertMsg({message: 'Login Failed. Incorrect email and/or password', variant: 'danger'});
+                setLoginAlertMsg({ message: 'Login Failed. Incorrect email and/or password', variant: 'danger' });
             } else {
-                setLoginAlertMsg({message: err.message, variant: 'danger'});
+                setLoginAlertMsg({ message: err.message, variant: 'danger' });
             }
         }
     };
@@ -71,7 +72,7 @@ export const LoginForm: React.FC<LoginFormProps> = () => {
             console.log(form);
             handleLogin();
         }
-  
+
         setValidated(true);
     };
 
@@ -86,38 +87,49 @@ export const LoginForm: React.FC<LoginFormProps> = () => {
     });
 
     return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit} action='#'>
-            <Form.Group controlId="institutionalEmail">
-                {(loginAlertMsg !== '') && <Alert variant={registrationAlertType}>{loginAlertMsg}</Alert>}
-                <Form.Label>Institutional Email Address</Form.Label>
-                <Form.Control
-                    required
-                    defaultValue='' 
-                    name="email" 
-                    autoComplete="username" 
-                    type="email" 
-                    placeholder="cxavier@xavierinstitute.edu"
-                    onChange={handleNamedChange('email')}
-                />
-                <Form.Control.Feedback type="invalid">{<span>An Institutional is required.</span>}</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    required
-                    defaultValue='' 
-                    name="password" 
-                    autoComplete="current-password" 
-                    type="password" 
-                    placeholder="******" 
-                    onChange={handleNamedChange('password')}
-                />
-                <Form.Control.Feedback type="invalid">{<span>A password is required.</span>}</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group>
-                <Button type="submit">Submit</Button>
-            </Form.Group>
-        </Form>
+        <>
+            <Form noValidate validated={validated} onSubmit={handleSubmit} action='#'>
+                <Form.Group controlId="institutionalEmail">
+                    {(loginAlertMsg !== '') && <Alert variant={registrationAlertType}>{loginAlertMsg}</Alert>}
+                    <Form.Label>Institutional Email Address</Form.Label>
+                    <Form.Control
+                        required
+                        defaultValue=''
+                        name="email"
+                        autoComplete="username"
+                        type="email"
+                        placeholder="cxavier@xavierinstitute.edu"
+                        onChange={handleNamedChange('email')}
+                    />
+                    <Form.Control.Feedback type="invalid">{<span>An Institutional is required.</span>}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        defaultValue=''
+                        name="password"
+                        autoComplete="current-password"
+                        type="password"
+                        placeholder="******"
+                        onChange={handleNamedChange('password')}
+                    />
+                    <Form.Control.Feedback type="invalid">{<span>A password is required.</span>}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group>
+                    <Button type="submit">Submit</Button>
+                </Form.Group>
+            </Form>
+            <Row>
+                <Col className="text-center">
+                    <ButtonAndModal
+                        buttonText="Forgot password"
+                        header={<h1>Forgot Password</h1>}
+                        variant="link"
+                    ></ButtonAndModal>
+                </Col>
+            </Row>
+        </>
     );
 };
 
