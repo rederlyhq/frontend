2d5d81cae007eb542caa68521a1fdff4bdabb465
red-tt-bear <red.tt.bear@gmail.com>
2020-08-27 20:37:36 -0400

    RED-52 Configured homepage to tabs rather than modals

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,36 +1,52 @@
-import React from 'react';
-import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Jumbotron, Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import LoginForm from '../Login/LoginForm';
 import RegisterForm from '../Login/RegisterForm';
-import ButtonAndModal from '../Components/ButtonAndModal';
 
 import './HomePage.css';
+
+enum HomeDetailsTabs {
+    LOGIN = 'Login',
+    REGISTER = 'Register',
+}
 
 interface HomePageProps {
 
 }
 
 export const HomePage: React.FC<HomePageProps> = () => {
+    const [activeTab, setActiveTab] = useState<HomeDetailsTabs>(HomeDetailsTabs.LOGIN);
+
     return (
         <>
             <Jumbotron id="HomePageJumbo" fluid>
                 <h1 id="JumboText">Rederly Coursework</h1>
             </Jumbotron>
-            
+
             <Container>
                 <Row>
-                    <Col md={{span: 4, offset: 4}} className="text-center">
+                    <Col md={{ span: 4, offset: 4 }}>
                         <h3>Rederly Coursework</h3>
-                        <Row className="justify-content-sm-center">
-                            <ButtonAndModal header="Log In" buttonText="Log In">
-                                <LoginForm/>
-                            </ButtonAndModal>
-                            <ButtonAndModal header="Register" buttonText="Register">
-                                <RegisterForm/>
-                            </ButtonAndModal>
-                        </Row>
+                        <Tabs
+                            activeKey={activeTab}
+                            defaultActiveKey={HomeDetailsTabs.LOGIN}
+                            id="home-page-tabs"
+                            onSelect={(activeTab: any) => {
+                                setActiveTab(activeTab);
+                            }}
+                            style={{
+                                marginTop: '20px'
+                            }}
+                        >
+                            <Tab eventKey={HomeDetailsTabs.LOGIN} title={HomeDetailsTabs.LOGIN} style={{ marginBottom: '10px' }}>
+                                <LoginForm />
+                            </Tab>
+                            <Tab eventKey={HomeDetailsTabs.REGISTER} title={HomeDetailsTabs.REGISTER} style={{ marginBottom: '10px' }}>
+                                <RegisterForm />
+                            </Tab>
+                        </Tabs>
                         <Row>
-                            <Col style={{margin: '10px'}}>
+                            <Col style={{ margin: '10px' }} className="text-center">
                                 <a href="https://rederly.com/privacy-policy" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
                             </Col>
                         </Row>
