bb7c6b91c6601750849c799f4414ce2aa31553e9
red-tt-bear <red.tt.bear@gmail.com>
2020-09-05 00:12:54 -0400

    RED-99 Added some additional configurations to the checkbox hider ui
    Dictate if the label is stacked and if the checkbox is before or after
    the children

--- src/Components/CheckboxHider.tsx
+++ src/Components/CheckboxHider.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 
+export enum CheckboxHiderChildrenPosition {
+    BEFORE='BEFORE',
+    AFTER='AFTER'
+}
+
 interface CheckboxHiderProps {
     defaultChecked?: boolean;
     children: React.ReactNode;
     style?: React.CSSProperties;
     labelText: string;
     onChange?: (newValue: boolean) => void;
+    position?: CheckboxHiderChildrenPosition;
+    stackLabel?: boolean;
 }
 
 export const CheckboxHider: React.FC<CheckboxHiderProps> = ({
@@ -14,12 +21,14 @@ export const CheckboxHider: React.FC<CheckboxHiderProps> = ({
     children,
     style,
     labelText,
-    onChange
+    onChange,
+    position = CheckboxHiderChildrenPosition.BEFORE,
+    stackLabel = true
 }) => {
     const [checked, setChecked] = useState<boolean>(defaultChecked);
     return (
         <>
-            {checked && children}
+            {checked && position === CheckboxHiderChildrenPosition.BEFORE && children}
             <Form.Group
                 className="unhighlightable"
                 style={{
@@ -32,9 +41,10 @@ export const CheckboxHider: React.FC<CheckboxHiderProps> = ({
                     onChange?.(newValue);
                 }}
             >
+                {stackLabel && 
                 <Form.Label style={{
                     marginBottom: '0px'
-                }}>{labelText}</Form.Label>
+                }}>{labelText}</Form.Label>}
                 <Form.Check
                     style={{
                         textAlign: 'center'
@@ -45,8 +55,10 @@ export const CheckboxHider: React.FC<CheckboxHiderProps> = ({
                         setChecked(newValue);
                         onChange?.(newValue);
                     }}
+                    label={!stackLabel ? labelText : undefined}
                 />
             </Form.Group>
+            {checked && position === CheckboxHiderChildrenPosition.AFTER && children}
         </>
     );
 };
