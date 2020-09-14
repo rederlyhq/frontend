c6dd229abdfab20722e4f24bc39d9a0bb8bd1bbf
red-tt-bear <red.tt.bear@gmail.com>
2020-09-13 22:30:08 -0400

    RED-9 Added some preliminary phases and new grade input

--- src/Courses/CourseDetailsTabs/OverrideGradeModal.tsx
+++ src/Courses/CourseDetailsTabs/OverrideGradeModal.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form, FormControl, FormGroup, FormLabel, Modal } from 'react-bootstrap';
 import _ from 'lodash';
 import { StudentGrade } from '../CourseInterfaces';
 
+enum OverrideGradePhase {
+    PROMPT = 'PROMPT',
+    CONFIRM = 'CONFIRM',
+    LOCK = 'LOCK'
+}
 interface OverrideGradeModalProps {
     show: boolean;
     onHide: () => void;
@@ -14,11 +19,38 @@ export const OverrideGradeModal: React.FC<OverrideGradeModalProps> = ({
     onHide: onHideProp,
     grade
 }) => {
+    const [overrideGradePhase, setOverrideGradePhase] = useState<OverrideGradePhase>(OverrideGradePhase.PROMPT);
+    const [validated, setValidated] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+    const displayCurrentScore = (grade.effectiveScore * 100).toFixed(1);
+    const [newScorePercentInput, setNewScorePercentInput] = useState<string>(displayCurrentScore);
     const onHide = () => {
+        setOverrideGradePhase(OverrideGradePhase.PROMPT);
         onHideProp();
     };
-    
-    const displayScore = (grade.effectiveScore * 100).toFixed(1);
+
+    const onNewScoreChange = (ev: React.ChangeEvent<HTMLInputElement>): void => setNewScorePercentInput(ev.target.value);
+
+    const overrideGrade = () => {
+        const newScore = parseInt(newScorePercentInput, 10) / 100;
+        if (newScore === grade.effectiveScore) {
+            onHide();
+        }
+    };
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            overrideGrade();
+        }
+  
+        setValidated(true);
+    };
+
     return (
         <Modal
             show={show}
@@ -27,17 +59,48 @@ export const OverrideGradeModal: React.FC<OverrideGradeModalProps> = ({
             <Modal.Header closeButton>
                 <h6>Override Grade</h6>
             </Modal.Header>
-            <Modal.Body>
-                The student currently has a {displayScore}!
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onHide}>
-                    Cancel
-                </Button>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Modal.Footer>
+            <Form noValidate validated={validated} onSubmit={onSubmit}>
+
+                <Modal.Body>
+                    <p>The student currently has a score of <strong>{displayCurrentScore}</strong> on this problem.</p>
+                    <FormGroup controlId='override-score'>
+                        <FormLabel>
+                                New score:
+                        </FormLabel>
+                        <FormControl
+                            required
+                            value={newScorePercentInput}
+                            size='lg'
+                            readOnly={loading}
+                            type='number'
+                            min={0}
+                            max={100}
+                            onChange={onNewScoreChange}
+                        />
+                        <Form.Control.Feedback type="invalid">{<span>The new score must be a postive number between 0 and 100</span>}</Form.Control.Feedback>
+                    </FormGroup>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={onHide}>
+                            Cancel
+                    </Button>
+                    {overrideGradePhase === OverrideGradePhase.PROMPT &&
+                    <>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </>
+                    }
+                    {overrideGradePhase === OverrideGradePhase.LOCK &&
+                    <>
+                        <Button variant="danger" type="submit">
+                            Lock
+                        </Button>
+                    </>
+                    }
+                </Modal.Footer>
+            </Form>
         </Modal>
     );
 };
