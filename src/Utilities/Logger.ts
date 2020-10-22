fed0eb964ab63f1c054ea81d43f6ec45b2b04cc8
red-tt-bear <red.tt.bear@gmail.com>
2020-10-22 12:50:48 -0400

    RED-180 Fixed breakage in logging
    Solution switch from json to simple format

--- src/Utilities/Logger.ts
+++ src/Utilities/Logger.ts
@@ -1,35 +1,37 @@
-// import winston from 'winston';
-// import BrowserConsoleLoggerTransport from './BrowserConsoleLoggerTransport';
+import winston from 'winston';
+import BrowserConsoleLoggerTransport from './BrowserConsoleLoggerTransport';
 
-// let level = process.env.NODE_ENV === 'production' ? 'error' : 'debug';
+let level = process.env.NODE_ENV === 'production' ? 'error' : 'debug';
 
-// const transports = {
-//     console: new BrowserConsoleLoggerTransport(
-//         {
-//             format: winston.format.simple(),
-//             level: level,
-//         },
-//     ),
-// };
+const transports = {
+    console: new BrowserConsoleLoggerTransport(
+        {
+            format: winston.format.simple(),
+            level: level,
+        },
+    ),
+};
 
-// const logger = winston.createLogger({
-//     level: level,
-//     format: winston.format.json(),
-//     // defaultMeta: { service: 'user-service' },
-//     transports: Object.values(transports),
-// });
+const logger = winston.createLogger({
+    level: level,
+    format: winston.format.simple(),
+    defaultMeta: { service: 'user-service' },
+    transports: Object.values(transports),
+    
+});
 
-// declare global {
-//     interface Window {
-//         logger: winston.Logger;
-//         setLogLevel(level: string): void;
-//     }
-// }
+declare global {
+    interface Window {
+        logger: winston.Logger;
+        setLogLevel(level: string): void;
+    }
+}
 
-// window.logger = logger;
-// window.setLogLevel = (level: string) => {
-//     transports.console.level = level;
-// };
+window.logger = logger;
+window.setLogLevel = (level: string) => {
+    transports.console.level = level;
+};
 
-const logger = console;
+
+// const logger = console;
 export default logger;
