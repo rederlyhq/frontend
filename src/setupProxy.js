1e7893c36cc32882a30dccea766dffd6012d6bfe
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-11-24 12:59:38 -0500

    Replaced embed with img tag to handle images.
    This fixes problems on Chrome where images weren't taking up the full space available for them, and not scaling correctly when the embed was set to take up a full width.
    
    This also includes some notes about setting up proxies to S3 buckets and HTTPS errors.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -10,4 +10,13 @@ module.exports =  function(app) {
     app.use(createProxyMiddleware('/webwork2_files', {
         target: 'http://localhost:3000'
     }));
+
+    /* If you're hosting attachments in an S3 bucket, point this proxy to that bucket.
+       You may need to use `npm run start-https` and secure: true in the config
+       object below if you get SSL errors. */
+    // app.use(createProxyMiddleware('/work', {
+    //     target: '',
+    //     changeOrigin: true,
+    //     secure: true,
+    // }));
 };
