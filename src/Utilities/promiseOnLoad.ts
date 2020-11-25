b956d05d014c3f41bb91907bdb16a4297c62c0d1
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-11-25 13:45:23 -0500

    Added error handling so that the rejections if an img fails to load do not get caught globally.

--- src/Utilities/promiseOnLoad.ts
+++ src/Utilities/promiseOnLoad.ts
@@ -1,21 +1,20 @@
+import logger from './Logger';
+
 // Based on: https://medium.com/@samthor/js-callbacks-to-promises-541adc46c07c
 export const promiseOnLoad = (elementRef: HTMLElement) => new Promise(
     (resolve, reject) => {
-        console.log('Adding listeners to.', elementRef);
         elementRef.addEventListener('load', () => {
-            console.log('ON LOAD CALLED');
-            console.log(elementRef);
             resolve();
         });
         elementRef.addEventListener('error', () => {
-            console.log('ON error CALLED');
             reject();
         });
         elementRef.addEventListener('abort', () => {
-            console.log('ON abort CALLED');
             reject();
         });
     }
-);
+).catch(()=>{
+    logger.warn('An element failed to load and rejected promiseOnLoad.');
+});
 
 export default promiseOnLoad;
