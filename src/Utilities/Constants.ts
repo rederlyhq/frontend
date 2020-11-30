57b605a41172330f216e6829b00a704adef25dc8
red-tt-bear <red.tt.bear@gmail.com>
2020-11-30 09:46:52 -0500

    RED-306 switched to more recent problem path regex

--- src/Utilities/Constants.ts
+++ src/Utilities/Constants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
     Renderer: {
-        VALID_PROBLEM_PATH_REGEX: /^(Library|Contrib|webwork-open-problem-library|private\/our|private\/templates|private\/rederly).*\.pg$/
+        VALID_PROBLEM_PATH_REGEX: /^(Library|Contrib|webwork-open-problem-library|private\/our|private\/my|private\/templates|private\/rederly).*\.pg$/
     }
 };
