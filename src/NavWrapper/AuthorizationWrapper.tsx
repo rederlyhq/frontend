18afdbfe7f2043b6da79c73d0d0fabf5e9fa47e9
red-tt-bear <red.tt.bear@gmail.com>
2020-11-30 19:01:39 -0500

    RED-314 Implemented auth countdown modal

--- src/NavWrapper/AuthorizationWrapper.tsx
+++ src/NavWrapper/AuthorizationWrapper.tsx
@@ -6,7 +6,9 @@ import { CookieEnum } from '../Enums/CookieEnum';
 import { unauthorizedRedirect } from '../Enums/UserRole';
 import logger from '../Utilities/Logger';
 import _ from 'lodash';
-import { Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import moment from 'moment';
+import { checkIn } from '../APIInterfaces/BackendAPI/Requests/UserRequests';
 
 interface AuthorizationWrapperProps {
     children: React.ReactNode
@@ -24,10 +26,22 @@ export const AuthorizationWrapper: React.FC<AuthorizationWrapperProps> = ({
     //     return sessionCookie.expirationDate
     // });
 
+    const renewSession = async () => {
+        try {
+            await checkIn();
+            // This triggers a check immediately
+            // I thought I would need a delay but it seems to work fine
+            setTokenExpiration(new Date());
+        } catch(e) {
+            logger.error('Could not check in', e);
+        }
+    };
+
     // Went with polling from now
     // TODO optimize by embedding the expiration date into the cookie and having moment reacter
     const reactionTime = tokenExpiration.toMoment().add(1, 'second'); // Give the browser a buffer to clear the cookie
-    return <MomentReacter stopMoment={reactionTime} significantMoments={[reactionTime]} logTag={'auth check'}>
+    const warningTime = tokenExpiration.toMoment().subtract(5, 'minute');
+    return <MomentReacter stopMoment={reactionTime} significantMoments={[warningTime, reactionTime]} logTag={'auth check'}>
         {() => {
             const sessionCookie = Cookies.get(CookieEnum.SESSION);
             if(!sessionCookie) {
@@ -51,6 +65,19 @@ export const AuthorizationWrapper: React.FC<AuthorizationWrapperProps> = ({
                 }
             }
             return <>
+                <Modal
+                    show={moment().isSameOrAfter(warningTime)}
+                >
+                    <Modal.Header>
+                        <h5>Inactivity warning</h5>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Your session will end due to inactivity in <MomentReacter logTag="auth countdown" intervalInMillis={1000}>{() => <>{tokenExpiration.toMoment().formattedFromNow(false, 'm [minutes] [and] s [seconds]')}</>}</MomentReacter>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={renewSession}>Renew session</Button>
+                    </Modal.Footer>
+                </Modal>
                 {children}
             </>;
         }}
