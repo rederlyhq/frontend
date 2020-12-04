874f9715ac1a12c4eb908a499377e57d76f90abb
red-tt-bear <red.tt.bear@gmail.com>
2020-12-04 10:20:46 -0500

    RED-320 Added timeout for backend requests

--- src/Hooks/AxiosRequest.tsx
+++ src/Hooks/AxiosRequest.tsx
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: '/backend-api',
-    responseType: 'json'
+    responseType: 'json',
+    timeout: 180000 // 180 seconds
 });
