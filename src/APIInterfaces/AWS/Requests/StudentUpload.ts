386d979b429dd0552760049cb8593e290e512b6c
red-tt-bear <red.tt.bear@gmail.com>
2020-12-04 10:20:08 -0500

    RED-320 Added timeout for attachment uploads

--- src/APIInterfaces/AWS/Requests/StudentUpload.ts
+++ src/APIInterfaces/AWS/Requests/StudentUpload.ts
@@ -1,7 +1,11 @@
 import { AxiosResponse } from 'axios';
-import AxiosRequest from '../../../Hooks/AxiosRequest';
 import BackendAPIError from '../../BackendAPI/BackendAPIError';
 import { PutUploadWork } from '../RequestTypes/StudentUploadRequests';
+import axios from 'axios';
+
+const attachmentsAxios = axios.create({
+    timeout: 300000 // 300000 millis = 5 minues
+});
 
 export const putUploadWork = ({
     presignedUrl,
@@ -9,13 +13,15 @@ export const putUploadWork = ({
     onUploadProgress,
 }: PutUploadWork): Promise<AxiosResponse<any>> => {
     try {
-        return AxiosRequest.put(presignedUrl.toString(), file, {
+        return attachmentsAxios.put(presignedUrl.toString(), file, {
             headers: {
                 'Content-Type': file.type,
             },
             onUploadProgress: onUploadProgress
         });
     } catch (e) {
+        // TODO Don't think this should actually be backend api error since it is not coming from the backend thus won't follow basic format
+        // I checked and it shouldn't hurt anything it just makes it more difficult to trap appropriate errors
         throw new BackendAPIError(e);
     }
 };
