6c39ac213f2c85ff1dd2e2a7b911fbb398a15700
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-12-09 20:07:08 -0500

    Added web-vitals based on CRA4's web-vitals implementation.

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Router } from './Router';
 import './Extensions';
 import GlobalErrorBoundaryState from './Utilities/ErrorBoundaries/GlobalErrorBoundary';
 import { VersionCheck } from './Utilities/VersionCheck';
+import reportWebVitals from './Utilities/reportWebVitals';
 
 // if (process.env.NODE_ENV !== 'production') {
 //     var axe = require('react-axe');
@@ -22,6 +23,12 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
