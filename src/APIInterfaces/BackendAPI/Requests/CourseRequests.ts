10c79b4aa47df4ac0012eececa68a116179924d3
Victor Von Doom <68438287+doombot-exe@users.noreply.github.com>
2020-12-17 18:34:10 -0500

    use custom header for origin

--- src/APIInterfaces/BackendAPI/Requests/CourseRequests.ts
+++ src/APIInterfaces/BackendAPI/Requests/CourseRequests.ts
@@ -542,7 +542,18 @@ export const askForHelp = async ({
 }: AskForHelpOptions): Promise<AxiosResponse<BackendAPIResponse>> => {
     try {
         return await AxiosRequest.get(
-            url.resolve(COURSE_QUESTION_PATH, `${questionId}/openlab`)
+            url.resolve(COURSE_QUESTION_PATH, `${questionId}/openlab`),
+            {
+                headers: {
+                    /**
+                     * Forms send this field in the origin header, however that wasn't coming across with the axios request
+                     * Adding `origin` myself was getting stripped
+                     * Could not find solution online so used a custom header
+                     * Other headers don't work because they get modified by aws (between cloudfront and the load balancers)
+                     */
+                    'rederly-origin': window.location.origin,
+                }
+            }
         );
     } catch (e) {
         throw new BackendAPIError(e);
