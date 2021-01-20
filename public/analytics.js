8eb38d02878e127acad576fa8240f7ec87b0241b
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-01-20 17:56:26 -0500

    Ran analyticsjs through babel env.

--- public/analytics.js
+++ public/analytics.js
@@ -1,13 +1,16 @@
+'use strict';
+
 // Define the GA_ID globally so it can be referenced in tsx files.
 window.GA_TRACKING_ID = 'G-EJ3PQ8D4XL';
-
-const gaScript = document.createElement('script');
+var gaScript = document.createElement('script');
 gaScript.async = true;
-gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${window.GA_TRACKING_ID}`;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id='.concat(window.GA_TRACKING_ID);
 document.head.appendChild(gaScript);
-
 window.dataLayer = window.dataLayer || [];
-function gtag(){window.dataLayer.push(arguments);}
+
+function gtag() {
+    window.dataLayer.push(arguments);
+}
 
 gtag('js', new Date());
 gtag('config', window.GA_TRACKING_ID);
