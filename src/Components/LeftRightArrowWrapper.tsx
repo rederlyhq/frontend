a10f978c5143891652716d0b797aa0455e2147ce
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-02-10 14:46:55 -0500

    Added buttons around the sides of the simple problem page for left-right navigation.

---
+++ src/Components/LeftRightArrowWrapper.tsx
@@ -0,0 +1,56 @@
+import _ from 'lodash';
+import React from 'react';
+import { ProblemObject } from '../Courses/CourseInterfaces';
+import logger from '../Utilities/Logger';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import './LeftRightArrow.css';
+
+// I would've liked to make this generic, but it's not in our usual paradigm.
+// https://wanago.io/2020/03/09/functional-react-components-with-generic-props-in-typescript/
+interface LeftRightArrowWrapperProps {
+    list: Record<number, ProblemObject>;
+    selected: number | null;
+    setSelected: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+
+export const LeftRightArrowWrapper: React.FC<LeftRightArrowWrapperProps> = ({ list, setSelected, selected, children }) => {
+
+    if (_.isNil(selected)) return null;
+
+    // This specialized wrap only works for our Record/problemNumber structure.
+    const wrap = (size: number, n: number) => {
+        if (n < 1) return size;
+        else if (n > size) return 1;
+        else return n;
+    };
+
+    const length = Object.keys(list).length;
+
+    const setNewSelected = (incr: 1 | -1) => {
+        const currProb = list[selected];
+        const newProbNumber = wrap(length, currProb.problemNumber + incr);
+        const newProb = _.find(list, ['problemNumber', newProbNumber]);
+
+        if (_.isNil(newProb)) {
+            logger.warn(`${incr} button could not find a problem to go to.`);
+            return;
+        }
+
+        setSelected(newProb?.id);
+    };
+
+    return <>
+        <button className='left-right-btn left-right-btn-left' onClick={()=>setNewSelected(-1)}>
+            <ArrowBackIosIcon />
+        </button>
+        <div style={{width: '90%', display: 'inline-block', margin: '0px 3.5%'}}>
+            {children}
+        </div>
+        <button className='left-right-btn left-right-btn-right' onClick={()=>setNewSelected(1)}>
+            <ArrowForwardIosIcon />
+        </button>
+    </>;
+};
