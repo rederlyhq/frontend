d860543441b02a5c0cf1992ba0ca49346a08617a
red-tt-bear <red.tt.bear@gmail.com>
2021-04-21 13:12:51 -0400

    Fixed bad merge

--- src/index.tsx
+++ src/index.tsx
@@ -12,26 +12,6 @@ import MomentUtils from '@date-io/moment';
 import { ConfigProvider } from './Contexts/ConfigProvider';
 import { GlobalSnackbarProvider } from './Contexts/GlobalSnackbar';
 
-interface RederlyConfig {
-    paymentURL: string;
-}
-
-declare global {
-    interface Window {
-        rederlyConfig?: Promise<RederlyConfig | null>;
-    }
-}
-
-const getConfig = () => axios.get<RederlyConfig>(`/config.json?cache_bust=${new Date().getTime()}`)
-    .then(resp => resp.data)
-    .catch(err => {
-        logger.error('Failed to load frontend config file.', err);
-        return null;
-    });
-
-
-window.rederlyConfig = getConfig();
-
 if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_ENABLE_AXE === 'true') {
     const axe = require('react-axe');
     axe(React, ReactDOM, 1000);
