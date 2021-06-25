475d6909b61ad0f1068a00dbaacec94b5454d166
red-tt-bear <red.tt.bear@gmail.com>
2021-06-25 15:23:47 -0400

    Fixed pwd for windows
    Powershell does not support pwd so switching to cwd

--- utility-scripts/pwd.js
+++ utility-scripts/pwd.js
@@ -2,8 +2,10 @@
 
 /**
  * Cross platform pwd (currently used for docker volume of relative directory)
+ * Technically if the node changes directories this is wrong (that's the difference between pwd and cwd, pwd is static from the beginning)
+ * However since we don't do that just switching so that this works for pwershell since process.env.PWD is undefined on windows with powershell
  */
-const pwd = () => process.env.PWD;
+const pwd = () => process.cwd();
 
 module.exports = {
     pwd: pwd
